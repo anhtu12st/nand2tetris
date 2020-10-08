258a2045352b6ef751c50abae4c96bda5cf8e563
Phan Anh Tu <anhtu12st@Phans-MacBook-Pro.local>
2020-10-08 08:38:38 +0700

    First Commit

---
+++ projects/04/fill/Fill.asm
@@ -0,0 +1,57 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Fill.asm
+
+// Runs an infinite loop that listens to the keyboard input. 
+// When a key is pressed (any key), the program blackens the screen,
+// i.e. writes "black" in every pixel. When no key is pressed, the
+// program clears the screen, i.e. writes "white" in every pixel.
+
+  @stat
+  M=-1
+  D=0
+  @SETSCREEN
+  0;JMP
+
+(LOOP)
+  @KBD
+  D=M
+  @SETSCREEN
+  D;JEQ
+  D=-1
+
+(SETSCREEN)
+  @arg
+  M=D
+  @stat
+  D=D-M
+  @LOOP 
+  D;JEQ
+
+  @arg
+  D=M
+  @stat
+  M=D
+
+  @SCREEN 
+  D=A 
+  @8192
+  D=D+A 
+  @i 
+  M=D
+
+(SETLOOP)
+  @i
+  D=M-1
+  M=D
+  @LOOP
+  D;JLT
+
+  @stat
+  D=M
+  @i 
+  A=M 
+  M=D
+  @SETLOOP
+  0;JMP
